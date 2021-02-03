f97590935808b06e4ce9446805fcc5f5beaa1c54
Juan Carlos <jcnicora1986@gmail.com>
2021-02-02 22:04:04 -0300

    Ingreso a booking

---
+++ src/test/java/prueba_booking.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.BeforeTest;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class prueba_booking {
+
+    //WebDriver driver;
+
+    @Test
+    public void ingresarBooking(){
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.booking.com");
+        //System.out.println(driver.getTitle());
+        driver.close();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+
+}
